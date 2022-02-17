b848fcc1becb63c7f6a69fc6511e78ba1f70e9db
Ritik Nandanwar <nandanwarritik10@gmail.com>
2022-02-17 21:00:13 +0530

    added carousel

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,7 +1,36 @@
-import { Container, Typography } from "@mui/material";
-import React from "react";
+import { Container, Link, Typography } from "@mui/material";
+import { TrendingCoins } from "../config.api";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import AliceCarousel from "react-alice-carousel";
 import banner from "../assets/banner.jpg";
+const handleDragStart = (e) => e.preventDefault();
+
 const Banner = () => {
+  const [trending, setTrending] = useState([]);
+  const getTrendingCoins = async () => {
+    let d = await axios.get(TrendingCoins("USD"));
+    setTrending(d.data);
+    console.log(d.data);
+  };
+  useEffect(() => {
+    getTrendingCoins();
+  }, []);
+  const items = trending.map((coin) => {
+    return (
+      <Link to={`/single/${coin.id}`}>
+        <img src={coin.image} height="80" alt="" />
+      </Link>
+    );
+  });
+  const responsive = {
+    0: {
+      items: 2,
+    },
+    512: {
+      items: 4,
+    },
+  };
   return (
     <div>
       <img
@@ -23,7 +52,6 @@ const Banner = () => {
           fontFamily: "Monsterrat",
           top: "20%",
           marginLeft: "8vw",
-          //   left: "40%",
           color: "white",
           display: "flex",
           flexDirection: "column",
@@ -34,7 +62,17 @@ const Banner = () => {
           Crypto
         </Typography>
         <Typography>Track all crypto</Typography>
+        <AliceCarousel
+          mouseTracking
+          infinite
+          autoPlayInterval={1000}
+          animationDuration={1000}
+          items={items}
+          autoPlay
+          responsive={responsive}
+        />
       </Container>
+
       {/* </div> */}
     </div>
   );
